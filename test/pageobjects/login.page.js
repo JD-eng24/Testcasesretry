e594a50e3698150df5d3701f911f61d815dff97e
JacobDoanfunzies22 <JACOB.DOAN2195@STU.MTEC.EDU>
2024-11-25 09:42:39 -0700

    Updated Multiple tests

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -12,24 +12,44 @@ class LoginPage extends Page {
     get inputUsername () {
         return $('#user-name');
     }
-
     get inputPassword () {
         return $('#password');
     }
-
     get btnSubmit () {
         return $('input[id="login-button"]');
     }
-
     get burger () {
         return $('button[id="react-burger-menu-btn"]');
     }
-
     get addCart () {
-        return $('button[id="add-to-cart-sauce-labs-bike-light"]');
+        return $('button[data-test="add-to-cart-sauce-labs-backpack"]')
    }
     get Shoppingcart () {
         return $('a[class="shopping_cart_link"]');
+    }
+    get removeCart () {
+        return $('button[class="btn btn_secondary btn_small cart_button"]');
+    }
+    get Allthings() {
+        return  $('a#inventory_sidebar_link[data-test="inventory-sidebar-link"]');
+    }
+    get info() {
+        return $('a[id="about_sidebar_link"]');
+    }
+    get logout() {
+        return $('a[id="logout_sidebar_link"]');
+    }
+    get reset() {
+        return $('a[id="reset_sidebar_link"]');
+    }
+    get exit() {
+        return $('#react-burger-cross-btn');
+    }
+    get checkout() {
+        return $('button[id="checkout"]');
+    }
+    get continue() {
+        return $('button[data-test="continue-shopping"]');
     }
     /**
      * a method to encapsule automation code to interact with the page
@@ -47,23 +67,84 @@ class LoginPage extends Page {
          await this.burger.waitForDisplayed();
          await this.burger.click();
          await browser.pause(2000);
+        await this.exit.click();
+    }  
+    async openAboutAndLogin() {
+        await this.burger.click();
+        await this.info.waitForDisplayed();
+        await browser.pause(3000);
+        await this.info.click();
+        await browser.url('https://www.saucedemo.com/');
+        await this.inputUsername.setValue('standard_user');
+        await this.inputPassword.setValue('secret_sauce');
+        await this.btnSubmit.click();
+        await this.exit.click();
+    }
+
+    async appStartover() {
+        await this.addCart.click();
+        await this.Shoppingcart.click();
+        await this.burger.click();
+        await this.reset.waitForDisplayed();
+        await this.reset.click();
+    }
+        
+    async shoppingMore() {
+        await this.Shoppingcart.click();
+        await this.continue.waitForDisplayed();
+        await browser.pause(5000);
+        // await this.continue.click();
+        }
+
+    async Xsign() {
+        await this.burger.click();
+        await this.exit.waitForDisplayed();
+        await this.exit.click();
     }
     /**
      * function to check if the add to cart button is working
      */
+    
     async clickCartMenu() {
         await this.addCart.waitForDisplayed();
         await this.addCart.click();
-        await browser.pause(4000);
     }
 
-    async Wheelcarts() {
+    async openShoppingCart() {
         await this.Shoppingcart.waitForDisplayed();
         await this.Shoppingcart.click();
-        await browser.pause(2000);
+    }
+
+    async removeItem() {
+        await this.removeCart.waitForDisplayed();
+        await this.removeCart.click();;
+    }
+
+    async clickAllItems() {
+        await this.burger.click();
+        await this.Allthings.waitForDisplayed();
+        await browser.pause(3000);
+        await this.Allthings.click();
+    }
+
+    async logoutAndLogin() {
+        await this.burger.click();
+        await this.logout.waitForDisplayed();
+        await this.logout.click();
+        await this.inputUsername.setValue('standard_user');
+        await this.inputPassword.setValue('secret_sauce');
+        await this.btnSubmit.click();
+    }
+    
+        async Itemdone() {
+        await this.Shoppingcart.click();
+        await this.checkout.waitForDisplayed();
+        await this.checkout.click();
+        await this.burger.click();
+        await this.info.click();
     }
     /**
-     * overwrite specific options to adapt it to page object
+     * overwrite specific options to adapt it to page object 
      */
     open () {
         return super.open();
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -24,10 +24,20 @@ describe('Hamburger Menu Test', () => {
     });
 });
 
+describe('X button exit', () => {
+    it('should exit you out of the hamburger page', async () => {
+        // await LoginPage.open();
+       // await LoginPage.login('standard_user', 'secret_sauce')
+       await LoginPage.Xsign();
+    })
+})
+
 describe('Add cart button test', () => {
     it('Added item should be added.', async () => {
         // await LoginPage.open();
         // await LoginPage.login('standard_user', 'secret_sauce')
+        await browser.pause(10000); 
+        // await LoginPage.Xsign.click();
         await LoginPage.clickCartMenu();
     })
 })
@@ -36,6 +46,56 @@ describe('Cart Testing', () => {
     it('should be clickable to go to your cart menu', async () => {
         // await LoginPage.open();
         // await LoginPage.login('standard_user', 'secret_sauce')
-        await LoginPage.Wheelcarts();
+        await LoginPage.openShoppingCart();
+    })
+})
+
+describe('Removing item test', () => {
+    it('should remove the item from your cart', async () => {
+        // await LoginPage.open();
+       // await LoginPage.login('standard_user', 'secret_sauce')
+       await LoginPage.removeItem();
+    })
+})
+
+
+describe('info testing', () => {
+    it('should see what the about is all about in saucedemo', async () => {
+        // await LoginPage.open();
+       // await LoginPage.login('standard_user', 'secret_sauce')
+       await LoginPage.logoutAndLogin();
+    })
+})
+
+describe('sign out test', () => {
+    it('should sign you out from the website', async () => {
+        // await LoginPage.open();
+       // await LoginPage.login('standard_user', 'secret_sauce')
+       await LoginPage.logoutAndLogin();
+    })
+})
+
+describe('Reset test', () => {
+    it('should reset the app state on saucedemo', async () => {
+        // await LoginPage.open();
+       // await LoginPage.login('standard_user', 'secret_sauce')
+       await LoginPage.appStartover();
+    })
+})
+
+
+describe('Continuing shop test', () => {
+    it('should return to the home page to add more items to your cart', async () => {
+        // await LoginPage.open();
+       // await LoginPage.login('standard_user', 'secret_sauce')
+       await LoginPage.shoppingMore();
+    })
+})
+
+describe('checkout test', () => {
+    it('should checkout you out and info added before you buy it', async () => {
+        // await LoginPage.open();
+       // await LoginPage.login('standard_user', 'secret_sauce')
+       await LoginPage.Itemdone();
     })
 })
